5440109a40f20bd57c5ec0060a99a1e6729c1026
Raj-Keshri398 <keshriraj093@gmail.com>
2025-01-29 16:25:55 +0000

    Knowing the variables and conversion

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+let score = "22"
+console.log(typeof score);
+
+let valueInnumber = Number(score);
+console.log(typeof valueInnumber); //here is the change the any string type number of value in different datatypes with the use of Conversion like string to number, float, double etc..
+
+let scores = "223hug"
+let valueInNum = Number(scores);
+console.log(valueInNum); //when the number with alphabet and convert the number in proper number and output will show NaN and typeof will show Number.
+
+// "33" => 33
+// "3345gd" => NaN
+// "true" => 1 , "false" => 0
+// "undefined" => NaN
+// "null" => 0
+//when we declare these type of things then the output will show this type...
+
+let isLoggedIn = 1;
+let booleanInLogged = Boolean(isLoggedIn);
+console.log(booleanInLogged);
+
+// 1 => true
+// 0 => false
+// "" empty string => false
+// "hello" any string => true
+
+let a = 33;
+let StingNumber = String(a);
+console.log(typeof StingNumber);
+
+
